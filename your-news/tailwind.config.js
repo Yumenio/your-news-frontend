35e36f0cc3ae9243500fa2d9913d3963c5093ff2
Yumenio <alejandroandy447@gmail.com>
2024-01-22 18:04:39 -0500

    feat: added custom fonts to tailwind.config file

---
+++ your-news/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      times: ['"times"', "sans-serif"],
+      cheltenhamItalicBold: ["cheltenhamItalicBold", "sans-serif"],
+      cheltenhamBold: ["cheltenhamBold", "sans-serif"],
+      cheltenhamLight: ["cheltenhamLight", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
