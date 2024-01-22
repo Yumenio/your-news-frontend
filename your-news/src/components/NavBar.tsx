98eaa920471ad39c7fb40b01186dd88a5526e1b6
Yumenio <alejandroandy447@gmail.com>
2024-01-22 18:07:58 -0500

    feat: added simple NavBar

---
+++ your-news/src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="w-full">
+      <div className="h-20 flex items-center">
+        <div className="mx-auto">
+          <h1 className="text-black text-4xl font-times">Your News</h1>
+        </div>
+      </div>
+      <hr className="border-black"/>
+    </div>
+  )
+}
+
+export default NavBar
