219d3164b3b7ae7e74e6f29e84f865550ffa5127
Yumenio <alejandroandy447@gmail.com>
2024-01-22 18:17:19 -0500

    fix: fixed Footer font-size

--- your-news/src/components/Footer.tsx
+++ your-news/src/components/Footer.tsx
@@ -6,9 +6,9 @@ const Footer = ({copyright}:{copyright:string|undefined}) => {
         <div className="w-11/12 m-auto">
             <hr/>
             <div className="flex flex-col md:flex-row justify-between px-4">
-                <h1>{copyright}</h1>
-                <div className="text-center md:text-right">
-                    <a target="_blank" href="https://icons8.com/icon/NsSPf86pJEl8/new-york-times">New York Times</a> icon by <a className="underline" target="_blank" href="https://icons8.com">Icons8</a>
+                <h1 className='text-xs text-center md:text-base md:text-right'>{copyright}</h1>
+                <div className="text-sm text-center md:text-base md:text-right">
+                    Icons by <a className="underline" target="_blank" href="https://icons8.com">Icons8</a>
                 </div>
             </div>
         </div>
