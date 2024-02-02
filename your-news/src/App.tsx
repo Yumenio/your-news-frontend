2545d93d99ea5e7df7623903f2136516ed9e0b52
Yumenio <alejandroandy447@gmail.com>
2024-02-01 19:38:39 -0500

    feat: adds router and routes for login and home

--- your-news/src/App.tsx
+++ your-news/src/App.tsx
@@ -5,36 +5,18 @@ import axios from 'axios';
 import ArticlePreview, { News } from './components/ArticlePreview';
 import CategorySelector from './components/CategorySelector';
 import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
-  const [apiResponse, setApiResponse] = useState<News|null>(null);
-  const [category, setCategory] = useState(0);
-  const getData = () => {
-    axios(`https://localhost:7226/api/top-headlines?category=${category}`)
-    .then(response =>{
-      console.log(category, response.data)
-      setApiResponse(response.data)
-    })
-    .catch(error =>{
-      console.error(error)
-    })
-  }
-  useEffect(()=>{
-    getData();
-    console.log("App", category);
-  }, [category])
   return (
     <div className="App">
       <NavBar/>
-      <CategorySelector onSelectCategory={setCategory} />
-      <ol className='max-w-2xl mx-auto'>
-        {
-          apiResponse?
-            apiResponse.results.map(e => (<ArticlePreview key={e.title} article={e}/>)):
-            ""
-        }
-      </ol>
-      <Footer copyright={apiResponse?.copyright}/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element= {<Login/>}/>
+      </Routes>
     </div>
   );
 }
