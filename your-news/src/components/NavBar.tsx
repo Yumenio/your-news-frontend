3e70b7fdee7590186a5313cff5b9a146b25ead9b
Yumenio <alejandroandy447@gmail.com>
2024-02-13 16:46:41 -0500

    feat: adds a Link to the navbar for navigating to the home page

--- your-news/src/components/NavBar.tsx
+++ your-news/src/components/NavBar.tsx
@@ -16,7 +16,7 @@ const NavBar = ({setToken, token}:{setToken:Dispatch<SetStateAction<string>>, to
     <div className="w-full sticky top-0 bg-white shadow-md z-50 mb-4">
       <div className="h-20 flex flex-row items-center">
         <div className=" mx-8 md:mx-auto flex w-full md:w-2/5 justify-between">
-          <h1 className="text-black text-4xl font-times">Your News</h1>
+          <Link to="/" className="text-black text-4xl font-times">Your News</Link>
           <div className="rounded border border-gray-300">
             {!token || token == ''?(
               <Link to="/login" className="text-black font-times text-2xl p-1.5">Log In</Link>
